bafdac620ad4958b21d96003e3ffb434105095b7
somKrooz <somkrooz80@gmail.com>
2024-12-19 16:07:01 +0530

    Rewritten The Whole Thing With Both Windows and Linux Support

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
 import os
 import json
+import platform
+
+
+def CreatePath() -> str:
+    if platform.system() == "Windows":
+       return "C:/Users/somkr/Downloads/Wallpapers/anime"
+    
+    elif platform.system() == "Linux":
+        return "/home/somkrooz/Downloads/Wallpapers"
 
 default = {
-    "dir": "/home/somkrooz/Downloads/Wallpapers"
+    "dir": CreatePath()
 }
 
 def createConfig(path):
@@ -12,27 +21,46 @@ def createConfig(path):
       json.dump(default, config, indent=4)
       print("Generated config file")
 
-# Look for .config Directory
-path = ""
-ispath = False
-home = os.path.expanduser('~')
-for i in os.listdir(home):
-    if i.startswith(".config"):
+# Linux
+if platform.system() == "Linux": 
+    path = ""
+    ispath = False
+    home = os.path.expanduser('~')
+    for i in os.listdir(home):
+        if i.startswith(".config"):
+            path = os.path.join(home,".config")
+            ispath = True
+
+    if ispath == False:
+        os.mkdir(os.path.join(home,".config"))
         path = os.path.join(home,".config")
-        ispath = True
 
-if ispath == False:
-    os.mkdir(os.path.join(home,".config"))
-    path = os.path.join(home,".config")
-
-#Look For Krooz File 
-if(os.path.isdir(path)):
-    if(os.path.isdir(os.path.join(path , "Krooz"))):        
-        createConfig(os.path.join(path , "Krooz"))
+    if(os.path.isdir(path)):
+        if(os.path.isdir(os.path.join(path , "Krooz"))):        
+            createConfig(os.path.join(path , "Krooz"))
+        else:
+            krooz = os.mkdir(os.path.join(path , "Krooz"))
+            createConfig(os.path.join(path , "Krooz"))
+            print("Created Krooz Directory...")
     else:
-        krooz = os.mkdir(os.path.join(path , "Krooz"))
-        createConfig(os.path.join(path , "Krooz"))
-        print("Created Krooz Directory...")
+        for i in range(10):
+            print("the .config File doesn't Exist...")
+
+#Windows
+elif platform.system() == "Windows":
+    path = ""
+    ispath = False
+    home = os.path.expanduser('~')
+    for i in os.listdir(home):
+        if i.startswith("Krooz"):
+            path = os.path.join(home,"Krooz")
+            ispath = True
+
+    if ispath == False:
+        os.mkdir(os.path.join(home,"Krooz"))
+        path = os.path.join(home,"Krooz")
+    
+    createConfig(path)
+
 else:
-    for i in range(10):
-        print("the .config File doesn't Exist...")
+    raise NotImplementedError(f"Not Implemented for: {platform.system()}")
